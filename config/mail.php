019c29defa81e1cf2ebe01dcb0c695184c3b4996
Santrix Admin <admin@santrix.com>
2026-01-05 13:45:12 +0700

    chore: update default mail sender to Santrix Owner

--- config/mail.php
+++ config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@santrix.my.id'),
+        'name' => env('MAIL_FROM_NAME', 'Santrix Owner'),
     ],
 
 ];
