1df03fbb7b8c58fe5726bd678b0f5b31abe33c2e
Santrix Admin <admin@santrix.com>
2026-01-06 23:30:21 +0700

    chore: cleanup react dependencies and files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,2 @@
 import './bootstrap';
-import * as Turbo from "@hotwired/turbo"
-
-Turbo.start()
-
-// Re-initialize Feather Icons on Turbo navigation
-document.addEventListener("turbo:load", () => {
-    if (typeof feather !== 'undefined') {
-        feather.replace();
-    }
-});
+import '../css/app.css'; 
