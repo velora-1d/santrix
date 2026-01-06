7323538cfd1a0c80e8a8e74900414da69205f60c
Santrix Admin <admin@santrix.com>
2026-01-06 22:11:38 +0700

    fix(react): add React import and StrictMode wrapper to fix error #31

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -14,7 +15,11 @@ createInertiaApp({
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
-        root.render(<App {...props} />);
+        root.render(
+            <React.StrictMode>
+                <App {...props} />
+            </React.StrictMode>
+        );
     },
     progress: {
         color: '#4f46e5',
