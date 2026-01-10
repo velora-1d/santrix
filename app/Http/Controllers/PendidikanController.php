ae68c22c6c185759c8e958344cb704ff1372257a
Santrix Admin <admin@santrix.com>
2026-01-10 13:43:17 +0700

    Fix: Add missing Auth facade import to PendidikanController

--- app/Http/Controllers/PendidikanController.php
+++ app/Http/Controllers/PendidikanController.php
@@ -9,6 +9,7 @@ use App\Models\NilaiSantri;
 use App\Models\JadwalPelajaran;
 use App\Models\TahunAjaran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
